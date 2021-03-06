c7383660266470c033d5658014f8053ac2e4b92e
Jon3Lee <3jonlee3@gmail.com>
2021-03-06 15:26:03 -0800

    Update lab5.c

--- ECE3301L/Lab 5/lab5.c
+++ ECE3301L/Lab 5/lab5.c
@@ -14,18 +14,18 @@
 char array[10] = (0x01, 0x4F, 0x12, 0x06, 0x4C, 0x24, 0x20, 0x0F, 0x00, 0x04);
 
 void Init_ADC(void) 
-    {
-        ADCON0 = 0x??;                      // select channel AN0, and turn on the ADC subsystem
-        ADCON1 = 0x?? ;                     // select pins AN0 through AN3 as analog signal, VDD-VSS as
+{
+    ADCON0 = 0x00;                          // select channel AN0, and turn on the ADC subsystem
+    ADCON1 = 0x00;                         // select pins AN0 through AN3 as analog signal, VDD-VSS as
                                             // reference voltage
-        ADCON2 = 0xA9;                      // right justify the result. Set the bit conversion time (TAD) and
+    ADCON2 = 0xA9;                          // right justify the result. Set the bit conversion time (TAD) and
                                             // acquisition time
-    }
+}
 
 
 void WAIT_1_SEC() 
 {
-    for (int j=0; i<0x17000; j++);
+    for (int j=0; j<17000; j++);
 }
 
 void Display_Lower_Digit(char digit)
@@ -36,23 +36,33 @@ void Display_Lower_Digit(char digit)
 void Display_Upper_Digit(char digit)
 {
     PORTC = array[digit];
+    if ()
 }
 
 void main(void)
 {
     Init_ADC();
+    init_UART();
     TRISA = 0X0F;                               //2 bits of port A connect to a LED, the others are input by temperature sensor and photoresistor
     TRISB = 0X00;                               //TRISB leads to a RGB LED so set to output
     TRISC = 0X00;                               //Set TRISC to output because it goes to a 7-Segment LED, which is always output
     TRISD = 0X00;                               //Set TRISD to output because it goes to a 7-Segment LED, which is always output
     TRISE = 0X00;                               //Set TRISE to output because it goes to a 7-Segment LED, which is always output
 
+    PORTA = 0x00;
+    PORTB = 0x00;
+    PORTC = 0xFF;
+    PORTD = 0xFF;
+    PORTE = 0xFF;
+
+
+
     while (1)
     {
         for (int i=0; i<10; i++)
         {
             Display_Lower_Digit(i);
-            Display_Upper_DIgit(i);
+            Display_Upper_Digit(i);
             WAIT_1_SEC();
         }
     }
