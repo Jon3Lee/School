5a28eac9f9f3f036d948d5c02ae431c330b0fc1a
Jon3Lee <3jonlee3@gmail.com>
2021-03-20 16:11:24 -0700

    Create lab7.c

---
+++ ECE3301L/Lab 7/lab7.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <xc.h>
+#include <math.h>
+#include <p18f4620.h>
+#include <usart.h>
+
+#pragma config OSC = INTIO67
+#pragma config WDT = OFF
+#pragma config LVP = OFF
+#pragma config BOREN = OFF
+#pragma config CCP2MX = PORTBE
+
