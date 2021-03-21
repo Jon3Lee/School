6bd8ddeed63c61e0450a2707b978ec03a7db4e8e
Jon3Lee <3jonlee3@gmail.com>
2021-03-20 23:02:17 -0700

    update

--- ECE3301L/Lab 7/lab7.c
+++ ECE3301L/Lab 7/lab7.c
@@ -13,6 +13,18 @@
 
 #define SEC_LED PORTDbits.RD7
 
+#define NS_RED PORTAbits.RA5
+#define NS_GREEN PORTBbits.RB0
+
+#define NSLT_RED PORTBbits.RB1
+#define NSLT_GREEN PORTBbits.RB2
+
+#define EW_RED PORTBbits.RB4
+#define EW_GREEN PORTBbits.RB5
+
+#define EWLT_RED PORTEbits.RE0
+#define EWLT_GREEN PORTEbits.RE2
+
 void init_UART();
 void putch(char);
 
@@ -50,8 +62,31 @@ void Wait_Half_Second()
     T0CONbits.TMR0ON = 0;                       // turn off the Timer 0
 }
 
+void Wait_N_Seconds (char seconds)
+{
+char I;
+    for (I = 0; I< seconds; I++)
+    {
+        Wait_One_Second();
+    }
+}
+
+void Set_NS(char color)
+{
+switch (color)
+{
+    case OFF: NS_RED =0;NS_GREEN=0;break;       // Turns off the NS LED
+    case RED: NS_RED =1;NS_GREEN=0;break;       // Sets NS LED RED
+    case GREEN: NS_RED =0;NS_GREEN=1;break;     // sets NS LED GREEN
+    case YELLOW: NS_RED =1;NS_GREEN=1;break;    // sets NS LED YELLOW
+}
+}
+
 void main(void)
 {
-
+    while (1)
+    {
+        
+    }
 }
 
