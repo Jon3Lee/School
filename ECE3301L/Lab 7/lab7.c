a3a2f8ef1d9597148b8120803fca1974aebaa3b2
Jon3Lee <3jonlee3@gmail.com>
2021-03-20 18:21:11 -0700

    Update lab7.c

--- ECE3301L/Lab 7/lab7.c
+++ ECE3301L/Lab 7/lab7.c
@@ -11,3 +11,21 @@
 #pragma config BOREN = OFF
 #pragma config CCP2MX = PORTBE
 
+void Wait_One_Second()
+{
+    SEC_LED = 0; // First, turn off the SEC LED
+    Wait_Half_Second(); // Wait for half second (or 500 msec)
+    SEC_LED = 1; // then turn on the SEC LED
+    Wait_Half_Second(); // Wait for half second (or 500 msec)
+}
+
+void Wait_Half_Second()
+{
+    T0CON = 0x02 // Timer 0, 16-bit mode, prescaler 1:8
+    TMR0L = 0x??; // set the lower byte of TMR
+    TMR0H = 0x??; // set the upper byte of TMR
+    INTCONbits.TMR0IF = 0; // clear the Timer 0 flag
+    T0CONbits.TMR0ON = 1; // Turn on the Timer 0
+    while (INTCONbits.TMR0IF == 0); // wait for the Timer Flag to be 1 for done
+    T0CONbits.TMR0ON = 0; // turn off the Timer 0
+}
