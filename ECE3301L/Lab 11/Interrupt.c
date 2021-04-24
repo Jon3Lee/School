7360f5df41a10c45928d700fb47292c59e43888e
Jon3Lee <3jonlee3@gmail.com>
2021-04-24 10:39:56 -0700

    create

---
+++ ECE3301L/Lab 11/Interrupt.c
@@ -0,0 +1,30 @@
+#include <xc.h>
+#include <p18f4620.h>
+#include "Interrupt.h"
+
+extern unsigned char Nec_state;
+extern short nec_ok;
+extern unsigned long long Nec_code;
+
+void interrupt high_priority chkisr()
+{
+ 
+}
+
+void TIMER1_isr(void)
+{
+
+}
+
+void force_nec_state0()
+{
+
+}
+
+void INT0_ISR() 
+{    
+                              
+} 
+
+
+
---
+++ ECE3301L/Lab 11/Lab11_sample.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <xc.h>
+#include <math.h>
+#include <p18f4620.h>
+#include <usart.h>
+
+#include "I2C.h"
+#include "I2C_Support.h"
+
+
+#pragma config OSC = INTIO67
+#pragma config BOREN =OFF
+#pragma config WDT=OFF
+#pragma config LVP=OFF
+
+char tempSecond = 0xff; 
+char second = 0x00;
+char minute = 0x00;
+char hour = 0x00;
+char dow = 0x00;
+char day = 0x00;
+char month = 0x00;
+char year = 0x00;
+char setup_second, setup_minute, setup_hour, setup_day, setup_month, setup_year;
+char alarm_second, alarm_minute, alarm_hour, alarm_date;
+char setup_alarm_second, setup_alarm_minute, setup_alarm_hour;
+
+char TempSecond;
+
+void INT0_ISR();
+
+
+void putch (char c)
+{   
+    while (!TRMT);       
+    TXREG = c;
+}
+
+void init_UART()
+{
+    OpenUSART (USART_TX_INT_OFF & USART_RX_INT_OFF & USART_ASYNCH_MODE & USART_EIGHT_BIT & USART_CONT_RX & USART_BRGH_HIGH, 25);
+    OSCCON = 0x70;
+}
+
+void init_INTERRUPT()
+{
+
+}
+
+void Do_Init()                      // Initialize the ports 
+{ 
+    init_UART();                    // Initialize the uart
+    OSCCON=0x70;                    // Set oscillator to 8 MHz 
+    
+    ADCON1=0x0F;
+
+    TRISB = 0x01;
+    TRISC = 0xFF;
+    TRISD = 0xFF;
+
+    RBPU=0;
+    
+    I2C_Init(100000); 
+    DS1621_Init();
+    init_INTERRUPT();
+    
+}
+
+void Wait_One_Sec()
+{
+    
+}
+
+void main() 
+{ 
+    Do_Init();                                                  // Initialization  
+    while (1)
+    {
+        char tempC = DS1621_Read_Temp();
+        char tempF = (tempC * 9 / 5) + 32;
+        printf (" Temperature = %d degreesC = %d degreesF\r\n", tempC, tempF);
+        Wait_One_Sec();
+    }
+    
+//    while (1)
+//    {
+//
+//        DS3231_Read_Time();
+//        if(tempSecond != second)
+//        {
+//            tempSecond = second;
+//            char tempC = DS1621_Read_Temp();
+//            char tempF = (tempC * 9 / 5) + 32;
+//            printf ("%02x:%02x:%02x %02x/%02x/%02x",hour,minute,second,month,day,year);
+//            printf (" Temperature = %d degreesC = %d degreesF\r\n", tempC, tempF);
+//        }
+//    }
+}
+
+
+
+
+
+
