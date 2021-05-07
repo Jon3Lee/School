eb281163961620195be8828b037a83a68083c47a
Jon3Lee <3jonlee3@gmail.com>
2021-05-06 20:34:52 -0700

    Update Lab12.c

--- ECE3301L/Lab 12/Lab12.c
+++ ECE3301L/Lab 12/Lab12.c
@@ -54,12 +54,12 @@ char time[]         = "00:00:00";
 char date[]         = "00/00/00";
 char alarm_time[]   = "00:00:00";
 char Alarm_SW_Txt[] = "OFF";
-char Fan_SW_Txt[]   = "OFF";                // text storage for Heater Mode
+char Fan_SW_Txt[]   = "OFF";                            // text storage for Heater Mode
 
     
-char DC_Txt[]       = "000";                // text storage for Duty Cycle value
-char RTC_ALARM_Txt[]= "0";                  //
-char RPM_Txt[]      = "0000";               // text storage for RPM
+char DC_Txt[]       = "000";                            // text storage for Duty Cycle value
+char RTC_ALARM_Txt[]= "0";                              //
+char RPM_Txt[]      = "0000";                           // text storage for RPM
 
 char setup_time[]       = "00:00:00";
 char setup_date[]       = "01/01/00";
@@ -80,17 +80,17 @@ void init_UART()
     OSCCON = 0x70;
 }
 
-void Do_Init()                      // Initialize the ports 
+void Do_Init()                                          // Initialize the ports 
 { 
-    init_UART();                    // Initialize the uart
-    OSCCON=0x70;                    // Set oscillator to 8 MHz 
+    init_UART();                                        // Initialize the uart
+    OSCCON=0x70;                                        // Set oscillator to 8 MHz 
     
-    ADCON1=0x0F;
-    TRISA = 0x00;
-    TRISB = 0x01;
-    TRISC = 0x01;
-    TRISD = 0x00;
-    TRISE = 0x00;
+    ADCON1=0x0F;                                        //Set ADCON1 to 0x0F
+    TRISA = 0x00;                                       //Set TRISA t0 0x00 since it is all outputs
+    TRISB = 0x01;                                       //Set TRISB to 0x01 since RB0 is input
+    TRISC = 0x01;                                       //Set TRISC to 0x01 since RC0 is input
+    TRISD = 0x00;                                       //Set TRISD to 0x00 since it is all outputs
+    TRISE = 0x00;                                       //Set TRISE to 0x00 since it is all outputs
 
 
     RBPU=0;
@@ -105,24 +105,24 @@ void Do_Init()                      // Initialize the ports
 
 void main() 
 {
-    Do_Init();                                                  // Initialization  
+    Do_Init();                                          // Initialization  
     Initialize_Screen();  
-    FAN_EN = 1;
-    duty_cycle = 50;
+    FAN_EN = 0;                                         //Start with fan turned off
+    duty_cycle = 50;                                    //And duty cycle set to 50
     do_update_pwm(duty_cycle);
     while (1)
     {
 
         DS3231_Read_Time();
 
-        if(tempSecond != second)
-        {
-            tempSecond = second;
-            DS1621_tempC = DS1621_Read_Temp();
-            DS1621_tempF = (DS1621_tempC * 9 / 5) + 32;
-            rpm = get_RPM();
-            Set_DC_RGB(duty_cycle);
-            Set_RPM_RGB(rpm);
+        if(tempSecond != second)                       //If tempsecond != second
+        {                       
+            tempSecond = second;                       //set tempsecond to second
+            DS1621_tempC = DS1621_Read_Temp();         //read temperature in Celcius
+            DS1621_tempF = (DS1621_tempC * 9 / 5) + 32;//Convert celcius to Faerenheight
+            rpm = get_RPM();                           //Get RPM an set variable rpm 
+            Set_DC_RGB(duty_cycle);                    //Change the LED color based on the value of duty_cycle
+            Set_RPM_RGB(rpm);                          //Change LED color based on value of rpm
             printf ("%02x:%02x:%02x %02x/%02x/%02x",hour,minute,second,month,day,year);
             printf (" Temp = %d C = %d F ", DS1621_tempC, DS1621_tempF);
             printf ("RPM = %d  dc = %d\r\n", rpm, duty_cycle);
@@ -135,11 +135,11 @@ void main()
 
             printf ("NEC_Code = %x\r\n", Nec_code1); // make sure to use Nec_code1
 
-            INTCONbits.INT0IE = 1;          // Enable external interrupt
-            INTCON2bits.INTEDG0 = 0;        // Edge programming for INT0 falling edge
+            INTCONbits.INT0IE = 1;                  // Enable external interrupt
+            INTCON2bits.INTEDG0 = 0;                // Edge programming for INT0 falling edge
 
-            found = 0xff;
-            for (int j=0; j< 21; j++)
+            found = 0xff;                           //
+            for (int j=0; j< 21; j++)               //
             {
                 if (Nec_code1 == array1[j]) 
                 {
@@ -152,29 +152,29 @@ void main()
             {
                 printf ("Cannot find button \r\n");
             }
-            else if (found == 6)
+            else if (found == 6)                  //If the button pressed is the - button
             {
-                Do_Beep();
-                printf ("button = %d \r\n", found);
-                Decrease_Speed();
+                Do_Beep();                        //Beep
+                printf ("button = %d \r\n", found);//Print found button
+                Decrease_Speed();                 //subtract 5 from duty_cycle
             }
-            else if (found == 7)
+            else if (found == 7)                 //If the button pressed is the + button
             {
-                Do_Beep();
+                Do_Beep();                      //Beep
                 printf ("button = %d \r\n", found);
-                Increase_Speed();
+                Increase_Speed();               //add 5 to duty_cycle
             }
-            else if (found == 5)
+            else if (found == 5)                //If the button pressed is the play/pause button
             {
-                Do_Beep();
+                Do_Beep();                      //Beep
                 printf ("button = %d \r\n", found);
-                Toggle_Fan();
+                Toggle_Fan();                   //Turn off/on fan
             }
-            else if (found == 8)
+            else if (found == 8)                //If the button pressed is the EQ button
             {
-                Do_Beep();
+                Do_Beep();                      //Beep
                 printf ("button = %d \r\n", found);
-                DS3231_Setup_Time();
+                DS3231_Setup_Time();            //Reset the time to 7:30
             }
 
         }
