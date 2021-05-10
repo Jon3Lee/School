7f0247be91561ea8c77f4d7b81e667c9d82d755f
Jon3Lee <3jonlee3@gmail.com>
2021-05-10 14:22:46 -0700

    Update Quiz3Q5.c

--- ECE4310/Quiz3Q5.c
+++ ECE4310/Quiz3Q5.c
@@ -1,19 +1,22 @@
+#define _CRT_SECURE_NO_DEPRECATE
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
     char ch;
-    FILE *src, *tgt;
+    FILE* src, * tgt;
     char srcf[] = "readhere.txt";
     char tgtf[] = "writehere.txt";
     src = fopen(srcf, "r");
-    tgt = fopen (tgtf, "w");
+    tgt = fopen(tgtf, "w");
 
     while ((ch = fgetc(src)) != EOF)
     {
         fputc(ch, tgt);
     }
+    printf("files copied!");
     fclose(src);
     fclose(tgt);
     return 0;
