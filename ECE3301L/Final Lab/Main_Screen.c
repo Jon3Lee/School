124164d5b446520842c8c1d10fefed048b808e25
Jon3Lee <3jonlee3@gmail.com>
2021-05-16 03:33:47 -0700

    Update Main_Screen.c

--- ECE3301L/Final Lab/Main_Screen.c
+++ ECE3301L/Final Lab/Main_Screen.c
@@ -154,8 +154,6 @@ void Update_Screen(void)
     Volt_Txt[0] = (int) volt + '0';
     Volt_Txt[2] = (((int)(volt*100))%100)/10 + '0';
     Volt_Txt[3] = ((int)(volt*100))%10 + '0'; 
-    // Volt_Txt[2] = ((((int)(volt))%100)/10) + '0';
-    // Volt_Txt[3] = (((int)(volt)%10)) + '0'; 
     
 
     RPM_Txt[0] = rpm/1000  + '0';
